691a4539deb3dc3064f9a27c1d18aeca1e4224ee
y-eren <84980503+y-eren@users.noreply.github.com>
2023-05-18 18:33:05 +0300

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $dbpass = "";
 $dbname = "proje";
 
 $db = mysqli_connect($dbhost, $dbuser, $dbpass,$dbname);
-
+mysqli_set_charset($db, "utf8");
 // if(mysqli_connect_errno())
 // {
 //     echo "Baglanti hatasi";
